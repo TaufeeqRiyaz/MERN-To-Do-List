c56b193a9e9c47b6013933380c79fd2933332d74
Taufeeq <taufeeq.riyaz64@gmail.com>
2024-07-03 12:11:41 +0530

    first commit

---
+++ src/components/array.js
@@ -0,0 +1,23 @@
+// Filename - components/array.js
+
+// Javascript object named array 
+// with 3 key-values
+const array = [
+    {
+        id: "1",
+        Name: "Taufeeq",
+        Age: "20",
+    },
+    {
+        id: "2",
+        Name: "Radhika",
+        Age: "21",
+    },
+    {
+        id: "3",
+        Name: "Pushan",
+        Age: "19",
+    },
+];
+
+export default array;
